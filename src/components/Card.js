7be7f0b2103954a6027f2c9e11df2387ea29aec9
rappos <emilemail4@gmail.com>
2020-03-19 12:09:50 +0100

    typing error

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ export default function Card() {
     <div className={style.cardBackground}>
       <h1 className={style.title}>Cat Fact</h1>
       {/** Style-applying of the fdct is done in the FactLoader file due to god knows what. 
-        Though it still is referencing the Card.module.css file*/}
+        Though it is still referencing the Card.module.css file*/}
       <FactLoader />
     </div>
   );
